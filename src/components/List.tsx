18aa5e55d12cb396c5583d16fc1b6bb21372ef9d
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-12-12 19:31:31 -0300

    render list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -10,20 +10,25 @@ interface Props {
    }>
 }
 
-const List = ({subs}: Props) => {
+const List: React.FunctionComponent<Props> = ({subs}) => {
+
+  const renderList = (): JSX.Element[] => {
+return subs.map(sub =>{
+  return(
+    <li key={sub.nick}>
+      <img src={sub.avatar} />
+      <h4>{sub.nick} (<small>{sub.subMonths}</small>)</h4>
+      {/* el signo "?" lo vamos a usar para asignarle que la descripcion tenga o no tenga una, que sea los primero 100 caracteres */}
+      <p>{sub.description?.substring(0,100)}</p>
+    </li>
+  )
+})
+  }
+
   return (
     <ul>
         {
-          subs.map(sub =>{
-            return(
-              <li key={sub.nick}>
-                <img src={sub.avatar} />
-                <h4>{sub.nick} (<small>{sub.subMonths}</small>)</h4>
-                {/* el signo "?" lo vamos a usar para asignarle que la descripcion tenga o no tenga una, que sea los primero 100 caracteres */}
-                <p>{sub.description?.substring(0,100)}</p>
-              </li>
-            )
-          })
+          renderList()
         }
       </ul>
   )
